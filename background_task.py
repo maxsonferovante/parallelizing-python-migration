1d69c10b98534f80db4ce4820fd0e0ed4cdd59db
maxsonferovante <maxsonferovante@gmail.com>
2024-04-29 10:53:15 -0300

    Corrigir tarefa em segundo plano para inserir usuários no PostgreSQL

--- background_task.py
+++ background_task.py
@@ -1,20 +1,25 @@
 import asyncio
 
-from models.settings.postgres.connection import db_postgres_connection_handler
+from models.settings.postgres.connection import PostgresConnectionHandler
 from models.repository.user_postgres_repository import UserPostgresRepository
 
-async def backend_task(data: list):
-    try:       
-        for user in data:
-            await UserPostgresRepository(
-                db_postgres_connection_handler.get_connection()
-                ).insert_user(
-                    username=user["username"],
-                    email=user["email"],
-                    age=user["age"]
-                )        
-        return len(data)
-    except Exception as e:
-        print(e)
+async def backend_task(child_conn):
+    while True:
+        message = await asyncio.get_event_loop().run_in_executor(None, child_conn.recv)
         
-    
+        if message == []:
+            break
+        try:
+            connection = PostgresConnectionHandler()
+            await connection.connect_to_db()
+            for user in message:
+                await UserPostgresRepository(
+                connection.get_connection()
+                ).insert_user(
+                                username=user["username"],
+                                email=user["email"],
+                                age=user["age"]
+                )     
+            await connection.close_connection()
+        except Exception as e:
+            print(f"Aqui está sendo gerado um erro: {e}")
